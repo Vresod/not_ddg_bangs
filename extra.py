7da7775dd9e27dc8a74112f29a8b12556964abfc
Vresod <lukebinkofsky@gmail.com>
2021-08-10 17:19:37 -0400

    add cool shortcuts, like bangs but for subdomains

--- extra.py
+++ extra.py
@@ -1,12 +1,25 @@
-from typing import Union
+from typing import Optional, Union
 import re
+from dataclasses import dataclass
 
-def extract_bang(bang_str:str,bangs:dict) -> Union[tuple,bool]:
+@dataclass
+class extracted_bang:
+	term:str
+	bang:Optional[str] = None
+	shortcut:Optional[str] = None
+	
+	def __bool__(self):
+		return bool(self.bang)
+
+def extract_bang(bang_str:str,bangs:dict[str],shortcuts:dict[str]) -> extracted_bang:
 	bang_str = bang_str.lower()
+	for shortcut in shortcuts:
+		if bang_str.startswith(f"!{shortcut}."):
+			return extracted_bang(bang=shortcut, shortcut=(d := bang_str.replace(f'!{shortcut}.','',1).split(" ",maxsplit=1))[0], term=d[1])
 	for bang in bangs:
 		if bang_str.split()[0] == f'!{bang}':
-			return bang, bang_str.replace(f'!{bang} ','',1)
-	return False
+			return extracted_bang(bang=bang, term=bang_str.replace(f'!{bang} ','',1))
+	return extracted_bang(bang_str)
 
 def get_root(url:str) -> Union[str,bool]:
 	regex = re.compile(r"(^https?:\/\/(www\.)?[\da-zA-Z\.-]+)")
@@ -14,3 +27,13 @@ def get_root(url:str) -> Union[str,bool]:
 	if not match:
 		return False
 	return match[0]
+
+
+if __name__ == "__main__":
+	def main():
+		import json
+		bangs = json.load(open("bangs.json"))
+		assert extract_bang("!hits bones",bangs['bangs'],bangs['shortcuts']) == extracted_bang("bones","hits")
+		assert extract_bang("!w.en bones",bangs['bangs'],bangs['shortcuts']) == extracted_bang("bones","w","en")
+		assert extract_bang("bones",bangs['bangs'],bangs['shortcuts']) == extracted_bang("bones")
+	main()
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import extra
 import urllib.parse
 
 app = Flask(__name__)
-with open("bangs.json") as bangsfile: bangs = json.loads(bangsfile.read())
+bangs = json.load(open("bangs.json"))
 engines = {"DuckDuckGo":"https://duckduckgo.com?q=%s","Google":"https://google.com/search?q=%s","Yahoo!":"https://search.yahoo.com/search?p=%s","Bing":"https://www.bing.com/search?q=%s"}
 
 @app.route('/search', methods=['GET'])
@@ -14,15 +14,17 @@ def search():
 	engine = request.args.get('engine') or 'DuckDuckGo'
 	ddg_if_bang = bool(request.args.get('dib'))
 	if not search: return redirect(url_for('root',e=5),code=307)
-	extracted = extra.extract_bang(search,bangs)
+	extracted = extra.extract_bang(search,bangs['bangs'],bangs['shortcuts'])
+
+	# for DIB
 	if ddg_if_bang and engine != 'DuckDuckGo' and search.startswith('!') and not extracted:
 		if json.loads(requests.get(f"https://api.duckduckgo.com/?q={search}&format=json&no_redirect=1").content).get('Redirect'):
 			return redirect(engines['DuckDuckGo'] % search)
-	if not extracted:
-		return redirect(engines[engine] % urllib.parse.quote(search),code=307)
-	if extracted[1] == search:
-		return redirect(extra.get_root(bangs[extracted[0]]),code=307)
-	return redirect(f'{bangs[extracted[0]] % extracted[1]}',code=307)
+	if extracted.shortcut:
+		return redirect(f'{bangs["shortcuts"][extracted.bang] % (extracted.shortcut,extracted.term)}',code=307)
+	if extracted.bang:
+		return redirect(f'{bangs["bangs"][extracted.bang] % extracted.term}',code=307)
+	return redirect(engines[engine] % urllib.parse.quote(search),code=307)
 
 @app.route('/raw_bangs',methods=['GET'])
 def get_bangs():
